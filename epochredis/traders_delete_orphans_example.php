327b02bee6e0d4ec8525a486a46b7e574116972b
Tim Rowland <tim@dwarfer.net>
2015-02-05 17:31:20 +0000

    orphan traders

--- epochredis/traders_delete_orphans_example.php
+++ epochredis/traders_delete_orphans_example.php
@@ -3,6 +3,8 @@
 	EpochMod Redis tools
 	http://www.deadmenrising.net/
 	By Dwarfer
+	
+	Example file to show deleting the Old AI_ITEMS once an AI Trader has been killed
 */
 
 require 'include/connect.inc.php';
@@ -14,7 +16,7 @@ $AI_ITEM_ID = ListRedisIDs('AI_ITEMS:*');
 
 //Checks to see if AI_ITEM_ID has extra data that is not in the AI_ID
 $AI_DIFF = array_diff_key($AI_ITEM_ID, $AI_ID);
-print "Found " . count($AI_DIFF) . " orphan AI_ITEMS\n";
+print "Found " . count($AI_DIFF) . " Orphan AI_ITEMS\n";
 foreach($AI_DIFF as $X=>$Y ){
 	//Deletes the old ai_items
 	print "Deleting $Y\n";
